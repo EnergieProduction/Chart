423b375ee69a93fc92c92552937000a76ee6cba7
De Gracia Mathieu <contact@degracia-mathieu.fr>
2017-04-16 13:15:47 +0200

    Ajout des méthodes dans la class PlotOption

--- src/option/PlotOption.php
+++ src/option/PlotOption.php
@@ -6,5 +6,18 @@ use EnergieProduction\Chart\Option\Option;
 
 Class PlotOption extends Option
 {
+	public function plotOption($value = null)
+	{
+		$this->options['plotOption'][__FUNCTION__] = $value;
+	}
 
+	public function shared($value = true)
+	{
+		$this->options['plotOption'][__FUNCTION__] = $value;
+	}
+
+	public function valueSuffix($value = null)
+	{
+		$this->options['plotOption'][__FUNCTION__] = $value;
+	}
 }
