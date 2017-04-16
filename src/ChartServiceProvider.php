cc88a06458ab0471838bd1448bc87050fa4f2af5
De Gracia Mathieu <contact@degracia-mathieu.fr>
2017-04-16 13:16:37 +0200

    Ajout des registres dans le provider

--- src/ChartServiceProvider.php
+++ src/ChartServiceProvider.php
@@ -15,9 +15,37 @@ class ChartServiceProvider extends ServiceProvider{
 
     public function boot()
     {
-        // Get namespace
-        $nameSpace = $this->app->getNamespace();
+        $this->app->bind('option.title', function($app) {
+            return new Builder(new Option\Title);
+        });
+
+        $this->app->bind('option.subtitle', function($app) {
+            return new Builder(new Option\Subtitle);
+        });
+
+        $this->app->bind('option.plotOption', function($app) {
+            return new Builder(new Option\PlotOption);
+        });
+
+        $this->app->bind('option.xAxis', function($app) {
+            return new Builder(new Option\XAxis);
+        });
+
+        $this->app->bind('option.yAxis', function($app) {
+            return new Builder(new Option\YAxis);
+        });
+
+        $this->app->bind('serie', function($app) {
+            return new Builder(new Serie\Serie);
+        });
+
+        $this->app->bind('chart', function($app) {
+            return new Chart();
+        });
     }
 
-    public function register() {}
+    public function register()
+    {
+        //
+    }
 }
