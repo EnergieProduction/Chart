e40ba8784db83f73347fc381f75f97a575b61422
De Gracia Mathieu <contact@degracia-mathieu.fr>
2017-05-27 13:29:25 +0200

    Ajout des méthodes accessibles pour les séries dans le fichier de config

--- src/Config/charts.php
+++ src/Config/charts.php
@@ -2,13 +2,18 @@
 
 return [
 
-	'available_types' => [
-		'title' => [],
-		'legend' => [],
-		'plotOption' => [],
-		'subtitle' => [],
-		'xAxis' => [],
-		'yAxis' => [],
+	'options' => [
+		'available_types' => [
+			'title' => ['text','align','style'],
+			'legend' => ['itemStyle'],
+			'plotOption' => ['plotOption','shared','valueSuffix'],
+			'subtitle' => ['text','style'],
+			'xAxis' => ['categories','tickWidth','gridLineWidth','labels'],
+			'yAxis' => ['text','label','showFirstLabel'],
+		],
 	],
 
+	'series' => [
+		'available_types' => ['name','data','zIndex','color','type','fillOpacity','lineWidth'],
+	]
 ];
