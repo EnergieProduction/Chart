458157d8d9228192fe14d63ed6a89ce38f9e69f1
DeGraciaMathieu <contact@degracia-mathieu.fr>
2017-06-02 21:58:12 +0200

    Suppression de la methode setDataAttribute de la class Serie

--- src/Highcharts/Series/Serie.php
+++ src/Highcharts/Series/Serie.php
@@ -6,8 +6,6 @@ use EnergieProduction\Chart\Highcharts\Highcharts;
 
 Class Serie extends Highcharts {
 
-	protected function setDataAttribute(array $value = [])
-	{
-		$this->options['data'] = $value;
-	}
+	//
+
 }
